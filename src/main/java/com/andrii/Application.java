08e21f881f36f192221409d650ea95a198a64e97
Andrii Alieksieienko <andriy.alieksieienko@gmail.com>
2026-06-17 11:42:18 +0300

    added constraints Table annotation and used Lombok annotation

--- src/main/java/com/andrii/Application.java
+++ src/main/java/com/andrii/Application.java
@@ -2,16 +2,17 @@ package com.andrii;
 
 import com.andrii.model.Book;
 import com.andrii.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.math.BigDecimal;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class Application {
-    @Autowired
-    private BookService bookService;
+    private final BookService bookService;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -24,6 +25,11 @@ public class Application {
             public void run(String... args) throws Exception {
                 Book book = new Book();
                 book.setTitle("Hook");
+                book.setAuthor("Arthur");
+                book.setIsbn("987-3-21");
+                book.setPrice(new BigDecimal("12.5"));
+                book.setDescription("Great");
+                book.setCoverImage("Hook");
 
                 Book savedBook = bookService.save(book);
 
--- src/main/java/com/andrii/service/impl/BookServiceImpl.java
+++ src/main/java/com/andrii/service/impl/BookServiceImpl.java
@@ -4,13 +4,13 @@ import com.andrii.model.Book;
 import com.andrii.repository.BookRepository;
 import com.andrii.service.BookService;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
-    @Autowired
-    private BookRepository bookRepository;
+    private final BookRepository bookRepository;
 
     @Override
     public Book save(Book book) {
