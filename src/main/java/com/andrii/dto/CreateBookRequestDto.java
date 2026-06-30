9e4c1dbd83ff73000b5ed89bb32f10db122b8715
Andrii Alieksieienko <andriy.alieksieienko@gmail.com>
2026-06-30 19:28:44 +0300

    added data validation and GlobalExceptionHandler

--- src/main/java/com/andrii/dto/CreateBookRequestDto.java
+++ src/main/java/com/andrii/dto/CreateBookRequestDto.java
@@ -1,5 +1,9 @@
 package com.andrii.dto;
 
+import com.andrii.validation.Isbn;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,9 +11,15 @@ import lombok.Setter;
 @Getter
 @Setter
 public class CreateBookRequestDto {
+    @NotNull
     private String title;
+    @NotNull
     private String author;
+    @NotBlank
+    @Isbn
     private String isbn;
+    @NotNull
+    @Min(0)
     private BigDecimal price;
     private String description;
     private String coverImage;
---
+++ src/main/java/com/andrii/validation/IsbnValidator.java
@@ -0,0 +1,15 @@
+package com.andrii.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import java.util.regex.Pattern;
+
+public class IsbnValidator implements ConstraintValidator<Isbn, String> {
+    private static final String PATTERN_OF_ISBN =
+            "^(?:\\d{9}[\\dX]|97[89]\\d{10})$";
+
+    @Override
+    public boolean isValid(String isbn, ConstraintValidatorContext constraintValidatorContext) {
+        return isbn != null && Pattern.compile(PATTERN_OF_ISBN).matcher(isbn).matches();
+    }
+}
