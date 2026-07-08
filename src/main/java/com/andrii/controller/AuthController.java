d550e5a43ed3f3bf8c8694c1755b117814093c90
Andrii Alieksieienko <andriy.alieksieienko@gmail.com>
2026-07-08 15:31:18 +0300

    added an AuthController description

--- src/main/java/com/andrii/controller/AuthController.java
+++ src/main/java/com/andrii/controller/AuthController.java
@@ -3,6 +3,8 @@ package com.andrii.controller;
 import com.andrii.dto.user.UserRegistrationRequestDto;
 import com.andrii.dto.user.UserResponseDto;
 import com.andrii.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Authentication management", description = "Endpoints for managing authentication")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/auth")
@@ -17,6 +20,7 @@ public class AuthController {
     private final UserService userService;
 
     @PostMapping("/registration")
+    @Operation(summary = "Register a new user")
     public UserResponseDto registerUser(@RequestBody @Valid UserRegistrationRequestDto requestDto) {
         return userService.register(requestDto);
     }
